73bde48182bd2f6f55a2e153cc4acbf3476f9706
Thomas Berryhill <thomas@berryhill.me>
2021-11-18 13:31:57 -0500

    implement day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,12 +1,47 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Day2 (day2) where
 
 import Common
+import Data.Function ((&))
+import Data.Maybe (fromMaybe, isNothing)
+import Data.Void (Void)
+import GHC.Ix (inRange)
+import Text.Megaparsec
+import Text.Megaparsec.Char
 
-part1 :: String -> String
-part1 _ = mempty
+data Policy = Policy
+  { passMin :: Int,
+    passMax :: Int,
+    passChar :: Char,
+    password :: String
+  }
+  deriving (Show)
 
-part2 :: String -> String
-part2 _ = mempty
+parseInput :: String -> [Policy]
+parseInput = fromMaybe [] . parseMaybe (many parsePolicy)
+  where
+    parsePolicy :: Parsec Void String Policy
+    parsePolicy = Policy <$> parseNum <*> parseNum <*> parseChar <*> parsePassword
+    parseNum = read <$> (some digitChar <* anySingle)
+    parseChar = lowerChar <* string ": "
+    parsePassword = some lowerChar <* many spaceChar
+
+part1 :: Policy -> Bool
+part1 Policy {..} = password & filter (== passChar) & length & inRange (passMin, passMax)
+
+part2 :: Policy -> Bool
+part2 Policy {..} = check passMin /= check passMax
+  where
+    check nth = (password !! (nth - 1)) == passChar
+
+testWith :: (Policy -> Bool) -> String -> String
+testWith isValidPolicy = show . length . filter isValidPolicy . parseInput
 
 day2 :: Day
-day2 = Day 2 [part1, part2]
+day2 = Day 2 [testWith part1, testWith part2]
+
+testInput =
+  "1-3 a: abcde\n\
+  \1-3 b: cdefg\n\
+  \2-9 c: ccccccccc"
